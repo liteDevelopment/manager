0f97f6d1470cacfb464a8dc28afbae45b61c9793
blade08 <cc411360770@sina.com>
2020-05-13 22:44:05 +0800

    添加销售单

--- doc/sql/init/init.sql
+++ doc/sql/init/init.sql
@@ -44,3 +44,33 @@ CREATE TABLE `user_role`
 ## 新增用户/账号
 INSERT INTO noah.user (id, name, password, phone) VALUES (1, 'admin', '$2a$10$PlfalHVD2vSeVTn9ou5QsOKyXGKJJTvNIIwuBuxN6CZKPcwedh5ly', '13010001000');
 INSERT INTO noah.account (id, account, type, user_id) VALUES (1, '13010001000', 1, 1);
+
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(32) NOT NULL COMMENT '学生姓名',
+  `contact` varchar(24) DEFAULT NULL COMMENT '联系方式',
+  `status` bit(1) DEFAULT NULL COMMENT '是否在校 0 不在 1 在',
+  `product_id` bigint(20) DEFAULT NULL COMMENT '所选课程id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '客户表';
+
+CREATE TABLE `product` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(64) NOT NULL COMMENT '课程名称',
+  `price` decimal(10,2) DEFAULT NULL COMMENT '产品价格',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '产品表';
+
+CREATE TABLE `sales_slip` (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL COMMENT '销售id',
+  `customer_id` bigint(20) NOT NULL COMMENT '客户id',
+  `product_id` bigint(20) NOT NULL COMMENT '产品id',
+  `num` decimal(5,0) NOT NULL COMMENT '产品数量',
+  `percentage` decimal(5,0) DEFAULT NULL,
+  `commission` decimal(5,0) DEFAULT NULL COMMENT '佣金',
+  `status` bit(1) DEFAULT NULL COMMENT '有效性',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '销售单表';
+
+
