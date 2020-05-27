6c138ccb5b96be91e2018b4c6e35527f7be10e45
blade08 <cc411360770@sina.com>
2020-05-28 00:22:07 +0800

    添加报表

--- src/main/java/com/handsome/manager/controller/ReportController.java
+++ src/main/java/com/handsome/manager/controller/ReportController.java
@@ -1,13 +1,18 @@
 package com.handsome.manager.controller;
 
+import com.handsome.manager.ao.ReportSaleDetailAO;
 import com.handsome.manager.service.ReportService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/pc/report")
@@ -17,10 +22,17 @@ public class ReportController {
     private ReportService reportService;
 
     @RequestMapping(value = "/saleDetails")
-    @ResponseBody
-    public Object saleDetails(Long userId, @DateTimeFormat(pattern = "yyyy-MM-dd") Date beginDate, @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
-
-        return reportService.getReportSaleDetails(userId, beginDate, endDate);
+    public Object saleDetails(Model model, Long userId, @DateTimeFormat(pattern = "yyyy-MM-dd") Date beginDate, @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
+        List<ReportSaleDetailAO> saleDetails = (List<ReportSaleDetailAO>) reportService.getReportSaleDetails(userId, beginDate, endDate).getData();
+        model.addAttribute("saleDetails", saleDetails);
+        BigDecimal totalCommission = new BigDecimal(0);
+        if (!CollectionUtils.isEmpty(saleDetails)) {
+            for (ReportSaleDetailAO saleDetail: saleDetails) {
+                totalCommission = totalCommission.add(saleDetail.getCommission());
+            }
+        }
+        model.addAttribute("totalCommission", totalCommission);
+        return "report/reportSaleDetails_table";
     }
 
 }
--- src/main/resources/static/js/report/reportSaleDetails.js
+++ src/main/resources/static/js/report/reportSaleDetails.js
@@ -1,3 +1,47 @@
 $(function () {
 
+    createSelect($("#userSelect"), "/manager/pc/user/select");
+
+    $("#btn-query").on("click", function () {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: "/manager/pc/report/saleDetails?userId=" + $("#userSelect").val() + "&beginDate=" + $("#beginDate").val() + "&endDate=" + $("#endDate").val(),
+            //data: $("#editForm").serialize(),
+            async: false,
+            error: function (request) {
+                alert("Server Connection Error.");
+            },
+            success: function (data) {
+                $("#reportTable").html(data);
+            }
+        });
+    });
+
+    function createSelect(elSelect, url, id) {
+        elSelect.empty();
+        $.ajax({
+            url: url,
+            type: 'get',
+            dataType: "json",
+            cache: "false",
+            success: function (data) {
+                if (data.code == 1 && data.data) {
+                elSelect.append("<option style='display: none'></option>");
+                    for (i = 0; i < data.data.length; i++) {
+                        if (id) {
+                            elSelect.append("<option value='"+data.data[i].value+"' "+ (data.data[i].value==id ? "selected" : "") + ">"+data.data[i].text+"</option>");
+                        } else {
+                            elSelect.append("<option value='"+data.data[i].value+"'>"+data.data[i].text+"</option>");
+                        }
+                    }
+                } else {
+                    alert(data.msg);
+                }
+            },
+            error: function (err) {
+                alert("Server Connection Error.");
+            }
+        });
+    }
 });
