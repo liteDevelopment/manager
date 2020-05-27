5085ec57e3b74d08e0a12349f7e95d7810b4f0f5
blade08 <cc411360770@sina.com>
2020-05-28 00:44:13 +0800

    调试

--- src/main/resources/static/js/report/reportSaleDetails.js
+++ src/main/resources/static/js/report/reportSaleDetails.js
@@ -3,11 +3,22 @@ $(function () {
     $._createSelect($("#userSelect"), "/manager/pc/user/select");
 
     $("#btn-query").on("click", function () {
+        var userId = $("#userSelect").val() ? $("#userSelect").val() : null;
+        var beginDate = $("#beginDate").val();
+        var endDate = $("#endDate").val();
+
+        if (!beginDate) { alert("请选择起始时间"); return; }
+        if (!endDate) { alert("请选截止时间"); return; }
+
+        var param = {};
+        param.beginDate = beginDate;
+        param.endDate = endDate;
+        userId && (param.userId = userId);
         $.ajax({
             cache: false,
             type: "POST",
-            url: "/manager/pc/report/saleDetails?userId=" + $("#userSelect").val() + "&beginDate=" + $("#beginDate").val() + "&endDate=" + $("#endDate").val(),
-            //data: $("#editForm").serialize(),
+            url: "/manager/pc/report/saleDetails",
+            data: param,
             async: false,
             error: function (request) {
                 alert("Server Connection Error.");
